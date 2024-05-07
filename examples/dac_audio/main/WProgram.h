533ef0e3b7cc0363bcf45a2e34df6a290241a7ad
Miso Kim <meeso.kim@gmail.com>
2024-05-07 20:16:33 +0900

    dac_audio/CMakeLists.txt dac_audio/README.md dac_audio/main/Adafruit_GFX.cpp dac_audio/main/Adafruit_GFX.h dac_audio/main/Adafruit_I2CDevice.h dac_audio/main/Adafruit_SPIDevice.h dac_audio/main/Arduino.h dac_audio/main/CMakeLists.txt dac_audio/main/ESP_8_BIT_GFX.cpp dac_audio/main/ESP_8_BIT_GFX.h dac_audio/main/ESP_8_BIT_composite.cpp dac_audio/main/ESP_8_BIT_composite.h dac_audio/main/Kconfig.projbuild dac_audio/main/Print.h dac_audio/main/Printable.h dac_audio/main/WProgram.h dac_audio/main/WString.h dac_audio/main/audio_example_file.h dac_audio/main/gfxfont.h dac_audio/pytest_dac_audio.py dac_audio/sdkconfig dac_audio/sdkconfig.ci.async dac_audio/sdkconfig.ci.sync dac_audio/sdkconfig.old dac_audio/tools/generate_audio_file.py dac_audio/tools/hi_idf_audio.wav

--- examples/dac_audio/main/WProgram.h
+++
@@ -1,6 +0,0 @@
-// typedef unsigned short uint16_t;
-// typedef short int16_t;
-// typedef unsigned int uint32_t;
-// typedef int int32_t;
-// typedef unsigned char uint8_t;
-// typedef char int8_t;
